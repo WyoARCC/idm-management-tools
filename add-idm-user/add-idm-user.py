caf09b2b14118f9778b0ad52f9b78efc32a61d56
Troy Axthelm <troy@slickrock.uwyo.edu>
2015-10-14 11:09:11 -0600

    changed from depricated optparse to argparse, added logging

--- add-idm-user/add-idm-user.py
+++ add-idm-user/add-idm-user.py
@@ -16,27 +16,43 @@
 #
 ###
 
-from optparse import OptionParser
+import argparse
+import logging
 
-usage = "usage: %prog [<username>[:<shell>]] [options]"
+__version__='1.0'
 
-parser = OptionParser(usage=usage)
+# argparser and auto generate help
 
-parser.set_defaults(verbose=True,logfile="/var/log/idm-log.log", dry=False)
+# add-idm-user usage
+usage = "%(prog)s [<username>[:<shell>] ]* [options]"
 
-parser.add_option(  "-f", "--file", dest="filename", 
+parser = argparse.ArgumentParser(prog='add-idm-user.py', usage=usage,
+                    description='Add users to idm with attributes from windows.uwyo.edu')
+
+# version
+parser.add_argument('--version', action='version', version="%(prog)s "+__version__)
+
+# default arg values
+parser.set_defaults(verbose=True,logfile="idm-actions.log", dry=False)
+
+# args
+parser.add_argument(  "-f", "--file", dest="filename", 
                     metavar="FILE", help="add users from FILE")
-parser.add_option(  "-s", "--shell", dest="defShell", 
+parser.add_argument(  "-s", "--shell", dest="defShell", 
                     metavar="SHELL", help="override default shell for added users \
                     valid shell options: bash, dash, tcsh")
-parser.add_option(  "-v", "--verbose", action="store_true", dest="verbose", 
+parser.add_argument(  "-v","--verbose", action="store_true", dest="verbose", 
+                    help="print status messages to stdout")
+parser.add_argument(  "-q", "--quiet", action="store_false", dest="verbose", 
                     help="don't print status messages to stdout")
-parser.add_option(  "-q", "--quiet", action="store_false", dest="verbose", 
-                    help="don't print status messages to stdout")
-parser.add_option(  "-n", "--dry-run", action="store_true", dest="dry",
+parser.add_argument(  "-n", "--dry-run", action="store_true", dest="dry",
                     help="run but do not add users to idm")
-parser.add_option(  "-l", "--logfile", dest="logfile", 
+parser.add_argument(  "-l", "--logfile", dest="logfile", 
                     help="change logfile location")
 
+# create parser
+args = parser.parse_args()
 
-(options,args) = parser.parse_args()
+logging.basicConfig(filename=args.logfile,level=logging.DEBUG)
+
+
