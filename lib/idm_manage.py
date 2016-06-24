e66b008e9327a6bc7a25865aedfb3b5330a01356
root <root@arcclab01.uwyo.edu>
2016-06-24 15:33:19 -0600

    Added mod-idm-radius and updates to use radius prod #TDA

--- lib/idm_manage.py
+++ lib/idm_manage.py
@@ -23,10 +23,10 @@ def addidmusers(userlist):
 	for user in userlist:
 		if user[5] == '':
 			idmcmd = ("ipa user-add %s --first=\"%s\" --last=\"%s\" --cn=\"%s\" --displayname=\"%s\" --email=\"%s\" --phone=\"%s\" --orgunit=\"%s\" --title=\"%s\"\
- --shell=\"%s\" --user-auth-type=radius --radius=radius-dev.uwyo.edu" % (user[0], user[1], user[2], user[3], user[3], user[4], user[7], user[8], user[9], user[10]))
+ --shell=\"%s\" --user-auth-type=radius --radius=radius-prod" % (user[0], user[1], user[2], user[3], user[3], user[4], user[7], user[8], user[9], user[10]))
 		else:
 			idmcmd = ("ipa user-add %s --first=\"%s\" --last=\"%s\" --cn=\"%s\" --displayname=\"%s\" --email=\"%s\" --uid=%s --gidnumber=%s --phone=\"%s\" --orgunit=\"%s\" --title=\"%s\"\
- --shell=\"/bin/%s\" --user-auth-type=radius --radius=radius-dev.uwyo.edu" % (user[0], user[1], user[2], user[3], user[3], user[4], user[5], user[6], user[7], user[8], user[9], user[10]))
+ --shell=\"/bin/%s\" --user-auth-type=radius --radius=radius-prod" % (user[0], user[1], user[2], user[3], user[3], user[4], user[5], user[6], user[7], user[8], user[9], user[10]))
 		
 		
 		print (idmcmd + "\n")
@@ -41,10 +41,10 @@ def printaddidmusers(userlist):
 	for user in userlist:
 		if user[5] == '':
 			idmcmd = ("ipa user-add %s --first=\"%s\" --last=\"%s\" --cn=\"%s\" --displayname=\"%s\" --email=\"%s\" --phone=\"%s\" --orgunit=\"%s\" --title=\"%s\"\
- --shell=\"%s\" --user-auth-type=radius --radius=radius-dev.uwyo.edu" % (user[0], user[1], user[2], user[3], user[3], user[4], user[7], user[8], user[9], user[10]))
+ --shell=\"%s\" --user-auth-type=radius --radius=radius-prod" % (user[0], user[1], user[2], user[3], user[3], user[4], user[7], user[8], user[9], user[10]))
 		else:
 			idmcmd = ("ipa user-add %s --first=\"%s\" --last=\"%s\" --cn=\"%s\" --displayname=\"%s\" --email=\"%s\" --uid=%s --gidnumber=%s --phone=\"%s\" --orgunit=\"%s\" --title=\"%s\"\
- --shell=\"/bin/%s\" --user-auth-type=radius --radius=radius-dev.uwyo.edu" % (user[0], user[1], user[2], user[3], user[3], user[4], user[5], user[6], user[7], user[8], user[9], user[10]))
+ --shell=\"/bin/%s\" --user-auth-type=radius --radius=radius-prod" % (user[0], user[1], user[2], user[3], user[3], user[4], user[5], user[6], user[7], user[8], user[9], user[10]))
 
 		print "\n"+idmcmd+"\n"
 
@@ -112,5 +112,14 @@ def addyubikey(username, yubikeyid):
 	print (idmcmd + "\n")
 	
 	idmaddresult = subprocess.Popen(idmcmd, stdout=subprocess.PIPE, shell=True)
+        idmaddresult = idmaddresult.communicate()[0]
+
+
+def modradius(username, radiusserv):
+	idmcmd = ("ipa user-mod  %s --radius=%s" % (username, radiusserv))
+
+        print (idmcmd + "\n")
+
+        idmaddresult = subprocess.Popen(idmcmd, stdout=subprocess.PIPE, shell=True)
         idmaddresult = idmaddresult.communicate()[0]
 
---
+++ mod-idm-radius.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+###
+#
+# add-idm-group.py
+#
+# Troy Axthelm
+# Advanced Research Computing Center
+# University of Wyoming
+# troy.axthelm@uwyo.edu
+#
+# Created: 21 October 2016
+#
+#
+# Modified: <initials> <day> <month> <year> <change notes>
+#
+###
+
+import sys
+sys.path.insert(0, '/root/idm-management-tools/lib/')
+import subprocess
+import argparse
+import logging
+import idm_manage
+
+__version__='1.0'
+
+# argparser and auto generate help
+
+# add-idm-group usage
+usage = "%(prog)s <new_rad_name>"
+
+parser = argparse.ArgumentParser(prog='mod-idm-radius.py', usage=usage,
+                    description='rad stuff')
+
+# default arg values
+parser.set_defaults(verbose=False,logfile="idm-actions.log")
+
+# args
+parser.add_argument(  "-v","--verbose", action="store_true", dest="verbose", 
+                    help="print additional status messages to stdout")
+parser.add_argument(  "-q", "--quiet", action="store_false", dest="verbose", 
+                    help="don't print status messages to stdout")
+parser.add_argument(  "-l", "--logfile", dest="logfile", 
+                    help="change logfile location")
+parser.add_argument(  'newrad', nargs='+')
+
+# create parser
+args = parser.parse_args()
+
+print args.newrad[0]
+
+cmd="ipa user-find | grep 'User login:' | cut -d : -f 2" 
+
+# run the cmd
+searchresult = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+searchresult = searchresult.communicate()[0].split()
+
+
+for user in searchresult:
+  idm_manage.modradius(user, args.newrad[0])
+  #ipacmd="ipa user-mod " + user + " --radius=" + args.newrad[0]
+  #print ipacmd
+
+  # run the ipacmd
+  #searchresult = subprocess.Popen(ipacmd, stdout=subprocess.PIPE, shell=True)
+  #searchresult = searchresult.communicate()[0].split()
